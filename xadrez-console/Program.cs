2f40d7df32416500886670a196538d7ea5d52b8d
LucianoMazar <lmazaraojr@gmail.com>
2025-05-19 16:23:31 -0300

    Criando a classe Posição

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,7 +1,9 @@
-﻿internal class Program
+﻿using tabuleiro;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Posicao posicao;
     }
 }
