a192dbb22c2de81de2dfc1e7d29043cd2aab2ddd
LucianoMazar <lmazaraojr@gmail.com>
2025-05-20 09:16:31 -0300

    Tratando erros com exeção personalizada

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,6 +1,4 @@
-﻿using tabuleiro;
-
-internal class Program
+﻿internal class Program
 {
     private static void Main(string[] args)
     {
--- xadrez-console/tabuleiro/Tabuleiro.cs
+++ xadrez-console/tabuleiro/Tabuleiro.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace tabuleiro
+﻿namespace tabuleiro
 {
     internal class Tabuleiro
     {
@@ -28,10 +22,38 @@ namespace tabuleiro
             return _pecas[linha, coluna];
         }
 
+        public Peca Peca(Posicao pos)
+        {
+            return _pecas[pos.Linha, pos.Coluna];
+        }
+
+        public bool existePeca(Posicao pos)
+        {
+            ValidarPosicao(pos);
+            return Peca(pos) != null;
+        }
+
         public void ColocarPeca(Peca p, Posicao pos)
         {
             _pecas[pos.Linha, pos.Coluna] = p;
             p.Posicao = pos;
         }
+
+        public bool PosicaoValida(Posicao pos)
+        {
+            if(pos.Linha < 0 || pos.Linha >= Linhas || pos.Coluna < 0 || pos.Coluna >= Colunas)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void ValidarPosicao(Posicao pos)
+        {
+            if (!PosicaoValida(pos))
+            {
+                throw new TabuleiroException("Posição Invalida");
+            }
+        } 
     }
 }
